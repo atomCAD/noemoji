6ea21619840389b4d23f993dca85135f3f8139cd
Mark Friedenbach <mark@friedenbach.org>
2025-11-19 21:58:31 +0000

    project: initialize noemoji linter for ASCII-first style enforcement
    Set up Rust project skeleton for noemoji, a linter that detects Unicode
    characters with ASCII equivalents (emojis, arrows, box drawing, etc).
    
    Rust project structure only - no linting functionality yet.
    
    MPL-2.0 licensed.

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file,
+// You can obtain one at <https://mozilla.org/MPL/2.0/>.
+
+//! Linter enforcing ASCII-first style guides by detecting Unicode with ASCII equivalents.
+//!
+//! `noemoji` detects Unicode characters that have ASCII equivalents (emojis, fancy arrows,
+//! checkmarks, box drawing, math symbols) and suggests appropriate replacements.
+
+// EOF
